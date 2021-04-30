e72447b3e529c9feefbc4ae9fa2d6e0b4453810b
Zach Chartrand <zachartrand999@gmail.com>
2021-04-30 14:06:49 -0400

    Edited the Quaternion class' docstring to be more specific.

--- quaternions.py
+++ quaternions.py
@@ -26,12 +26,15 @@ class Quaternion():
     together are known as the vector part. The vector part is made up
     of three (3) components whose unit values are i, j, and k. The
     rules for these values are as follows:
-    
+
         i**2 == j**2 == k**2 == -1
         i*j == -j*i == k
         j*k == -k*j == i
-        k*i == -i*k == j
-        i*j*k == -1
+        k*i == -i*k == j,
+
+    which leads to the following statement:
+
+        i*j*k == -1.
     """
     def __init__(self, real: int or float=0, i_component: int or float=0,
             j_component: int or float=0, k_component: int or float=0):
@@ -39,18 +42,18 @@ class Quaternion():
         self.i = float(i_component)
         self.j = float(j_component)
         self.k = float(k_component)
-    
+
     def __repr__(self):
         """
         Return repr(self).
-        
+
         Returns a string of the quaternion in the form
-        
+
             'a + bi + cj + dk',
-        
+
         where a, b, c, and d are floats. If there is no real component,
         it is left out of the string, and has the format
-        
+
             'bi + cj + dk'.
         """
         i_str = str(self.i) + 'i'
@@ -60,16 +63,16 @@ class Quaternion():
             q_str = ' + '.join([str(self.real), i_str, j_str, k_str])
         else:
             q_str = ' + '.join([i_str, j_str, k_str])
-        
+
         return f'({q_str})'
-    
+
     def __bool__(self):
         """self != 0"""
         if (self.real, self.i, self.j, self.k) == (0, 0, 0, 0):
             return False
-        
+
         return True
-    
+
     def __eq__(self, other):
         """Return self == other."""
         if isinstance(other, int) or isinstance(other, float):
@@ -80,13 +83,13 @@ class Quaternion():
         elif isinstance(other, Quaternion):
             return (self.real, self.i, self.j, self.k) == (
                 other.real, other.i, other.j, other.k)
-        
+
         return False
-    
+
     def __hash__(self):
         """
         Return hash(self).
-        
+
         Works so that scalar quaternions have the same hash values as
         int and float. Quaternions that have two of their imaginary
         terms equal to zero (0) have the same hash values as complex
@@ -98,14 +101,14 @@ class Quaternion():
             return hash(complex(self.real, self.get_imag()))
         else:
             return hash((self.real, self.i, self.j, self.k))
-    
+
     def __neg__(self):
         return Quaternion(-self.real, -self.i, -self.j, -self.k)
-    
+
     def __abs__(self):
         """Return abs(self)."""
         return _hypot(self.real, self.i, self.j, self.k)
-    
+
     def __add__(self, other):
         """Return self+other."""
         if isinstance(other, int) or isinstance(other, float):
@@ -124,7 +127,7 @@ class Quaternion():
         else:
             raise TypeError('Cannot add a {} to a {}'.format(
                 type(other).__name__, type(self).__name__))
-    
+
     def __radd__(self, other):
         """Return other+self."""
         if isinstance(other, int) or isinstance(other, float):
@@ -133,7 +136,7 @@ class Quaternion():
             raise TypeError(
                 'Cannot add a complex number to a Quaternion. Make the '
                 + 'complex number a Quaternion before adding.')
-    
+
     def __sub__(self, other):
         """Return self-other."""
         if isinstance(other, int) or isinstance(other, float):
@@ -152,7 +155,7 @@ class Quaternion():
         else:
             raise TypeError('Cannot add a {} to a {}'.format(
                 type(other).__name__, type(self).__name__))
-    
+
     def __rsub__(self, other):
         """Return other-self."""
         if isinstance(other, int) or isinstance(other, float):
@@ -162,14 +165,14 @@ class Quaternion():
             raise TypeError(
                 'Cannot subtract a Quaternion from a complex number. Make the '
                 + 'complex number a Quaternion before subtracting.')
-    
+
     def __mul__(self, other):
         """
         Return self*other.
-        
+
         If the Quaternion is multiplied by an int or float, the
         int/float is distributed to each component.
-        
+
         A Quaternion multiplied by a complex number will raise a
         TypeError, as it is ambiguous which vector component the
         imaginary component should be treated as.
@@ -193,9 +196,9 @@ class Quaternion():
         else:
             raise TypeError('Cannot multiply a {} by a {}'.format(
                 type(other).__name__, type(self).__name__))
-        
+
         return Quaternion(real, i, j, k)
-    
+
     def __rmul__(self, other):
         """Return other*self."""
         if isinstance(other, int) or isinstance(other, float):
@@ -204,21 +207,21 @@ class Quaternion():
             raise TypeError(
                 'Cannot multiply a complex number by a Quaternion. Make the '
                 + 'complex number a Quaternion before multiplying.')
-    
+
     def conjugate(self):
         """Returns the complex conjugate of self."""
         return Quaternion(self.real, -self.i, -self.j, -self.k)
-    
+
     def inverse(self):
         """Return 1/self."""
         q_inverse = self.conjugate() / (
             self.real**2 + self.i**2 + self.j**2 + self.k**2)
         return q_inverse
-    
+
     def __truediv__(self, other):
         """
         Return self/other.
-        
+
         For division q1/q2, this assumes the order of multiplication
         is q1 * 1/q2.
         """
@@ -231,7 +234,7 @@ class Quaternion():
                 + 'complex number a Quaternion before dividing.')
         elif isinstance(other, Quaternion):
             return self * other.inverse()
-    
+
     def __rtruediv__(self, other):
         """Return other/self."""
         if isinstance(other, int) or isinstance(other, float):
@@ -240,17 +243,17 @@ class Quaternion():
             raise TypeError(
                 'Cannot divide a complex number by a Quaternion. Make the '
                 + 'complex number a Quaternion before dividing.')
-    
+
     @property
     def scalar(self) -> float:
         """Return the real part of the quaternion."""
         return self.real
-    
+
     @property
     def vector(self) -> List[float]:
         """Return the vector part of the quaternion."""
         return Quaternion(0, self.i, self.j, self.k)
-    
+
     @property
     def unit_vector(self):
         """
@@ -262,15 +265,15 @@ class Quaternion():
         else:
             v = Quaternion(0, self.i, self.j, self.k)
             return v/abs(v)
-    
+
     def get_vector_components(self):
         """Return the vector components of the Quaternion as a list
         formatted as
-        
+
             [i, j, k].
         """
         return [self.i, self.j, self.k]
-    
+
     def is_scalar(self) -> bool:
         """
         Returns True if the vector components all equal zero.
@@ -279,7 +282,7 @@ class Quaternion():
         if (self.i, self.j, self.k) == (0, 0, 0):
             return True
         return False
-    
+
     def is_vector(self) -> bool:
         """
         Returns True if the scalar part is zero and at least one of
@@ -288,7 +291,7 @@ class Quaternion():
         if self.real == 0 and (self.i != 0 or self.j != 0 or self.k != 0):
             return True
         return False
-    
+
     def is_complex(self):
         """
         Returns True if only one of the i, j, and k components is
@@ -298,9 +301,9 @@ class Quaternion():
             if (0, 0) in (
                     (self.i, self.j), (self.j, self.k), (self.i, self.k)):
                 return True
-        
+
         return False
-    
+
     def get_imag(self):
         """
         Returns the imaginary component of the quaternion if only one
@@ -313,6 +316,6 @@ class Quaternion():
                     return component
         elif self.is_scalar():
             return 0.0
-        
+
         else:
             return None
