9d2a107d111f8d44dd075a7c5273ee1dc29a7454
Zach Chartrand <zachartrand999@gmail.com>
2022-03-03 21:55:36 -0500

    Formatting, comments, and miscellaneous changes.
    -Added comments explaining code.
    -Formatted doc strings and comments to be restricted to column 72 and 
    less.
    -Other code formatting.

--- quaternions/qmath.py
+++ quaternions/qmath.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Similar to the built-in module :py:mod:`cmath`, this module has definitions of
-mathematical functions expanded to work with quaternions.
+Similar to the built-in module :py:mod:`cmath`, this module has
+definitions of mathematical functions expanded to work with quaternions.
 """
 # Metadata
 # --------
 # Created on Wed Apr 28 21:23:28 2021
 # @author: Zach Chartrand <zachartrand999@gmail.com>
 
-__all__ = ['exp', 'log', 'log10', 'sqrt', 'rotate3d', 'pi', 'tau', 'e',
-           'inf', 'infi', 'infj', 'infk', 'nan', 'nani', 'nanj', 'nank']
+__all__ = ['exp', 'log', 'log10', 'sqrt', 'rotate3d', 'rotate_Euler',
+           'dot_product', 'cross_product', 'pi', 'tau', 'e', 'inf',
+           'infi', 'infj', 'infk', 'nan', 'nani', 'nanj', 'nank']
 
 from math import (
     exp as _exp,
@@ -36,46 +37,96 @@ infk: Quaternion = Quaternion(0, 0, 0, float('inf'))
 
 def exp(q: Quaternion or float) -> Quaternion:
     """Return the exponential of a quaternion."""
+    # ==================================================================
+    # The exponential (exp) of a quaternion (q) with a nonzero vector
+    # component is given by the formula
+    #
+    #     exp(q) = exp(||q||) * (cos(||v||) + v*sin(||v||)/||v||)
+    #
+    # where ||q|| is the magnitude of the quaternion, v is the vector
+    # part of the quaternion, and ||v|| is the magnitude of v. This
+    # algorithm avoids this formula if the vector part is zero (0) or
+    # if the input is an int or float.
+    # ==================================================================
     if isinstance(q, Quaternion):
         a = q.real
+        # If there is only a real component, skip all of the
+        # complicated mathematics.
         if q.is_scalar():
             return Quaternion(_exp(a), 0, 0, 0)
 
-        theta = q.vector_norm % tau
-        return _exp(a)*(_cos(theta) + q.unit_vector()*_sin(theta))
+        theta = q.vector_norm
+        unit_vector = q.unit_vector()
+        return _exp(a)*(_cos(theta) + unit_vector*_sin(theta))
 
     elif isinstance(q, (int, float)):
         return Quaternion(_exp(q), 0, 0, 0)
 
-    else:
-        return TypeError
+    # Raise a TypeError for complex numbers. Complex numbers should
+    # either use the cmath function or be converted to a quaternion
+    # before using this function.
+    elif isinstance(q, complex):
+        raise TypeError("Use the cmath module's exp() function or convert "
+                      + "the complex number to a quaternion.")
+
+    # Raise a TypeError if the input is not a real number or quaternion.
+    raise TypeError("Cannot take the quaternion exponential of a "
+                 + f"{q.__class__.__qualname__}.")
 
 
 def log(q: Quaternion or float, base: float = e) -> Quaternion:
     """
     Return the logarithm of a quaternion to the given base.
 
-    If the base is not specified, returns the natural logarithm (base *e*) of the
-    quaternion.
+    If the base is not specified, returns the natural logarithm
+    (base *e*) of the quaternion.
     """
+    # ==================================================================
+    # The natural logarithm of a quaternion (q) is given by the formula
+    #
+    #     log(q) = log(||q||) + v*phi/||v||
+    #
+    # where ||q|| is the norm of the quaternion, v is the vector part
+    # of the quaternion, ||v|| is the norm of the vector part of the
+    # quaternion, and phi is the angle of the quaternion given by
+    #
+    #     phi = acos(a/||q||),
+    #
+    # where acos is the inverse cosine function and a is the scalar
+    # part of the quaternion.
+    #
+    # Because log1p() is more accurate than log() for inputs close to
+    # 1.0, when the norm of the quaternion is between 0.71 and 1.73
+    # inclusive, log1p is automatically used. This range is used based
+    # on the cmath module, which uses log1p for complex numbers with
+    # magnitudes in this range.
+    # ==================================================================
     if isinstance(q, Quaternion):
+        real = q.real
+        # If the quaternion is real, use the normal logarithm functions.
         if q.is_scalar():
-            if 0.71 <= q.real and q.real <= 1.73:
-                return Quaternion(_log1p(q.real-1), 0, 0, 0)
+            if 0.71 <= real and real <= 1.73:
+                return Quaternion(_log1p(real-1), 0, 0, 0)
             else:
-                return Quaternion(_log(q.real), 0, 0, 0)
+                return Quaternion(_log(real), 0, 0, 0)
         else:
-            if 0.71 <= q.norm and q.norm <= 1.73:
-                abs_components = [abs(q.real), abs(q.i), abs(q.j), abs(q.k)]
+            norm = q.norm
+            if 0.71 <= norm and norm <= 1.73:
+                # This takes advantage of the difference of perfect
+                # squares to avoid potential loss of precision in
+                # calculating the norm - 1 of the quaternion. This
+                # formula is taken from the cmath log function,
+                # modified for four (4) components.
+                abs_components = [abs(real), abs(q.i), abs(q.j), abs(q.k)]
                 max_component = max(abs_components)
                 abs_components.remove(max_component)
                 real = _log1p(
-                    (max_component-1) * (max_component+1)
+                    (max_component - 1) * (max_component + 1)
                     + abs_components[0]*abs_components[0]
                     + abs_components[1]*abs_components[1]
                     + abs_components[2]*abs_components[2]) / 2.0
             else:
-                real = _log(q.norm)
+                real = _log(norm)
 
             angle = q.angle
             answer = real + q.unit_vector()*angle
@@ -90,8 +141,8 @@ def log(q: Quaternion or float, base: float = e) -> Quaternion:
     elif isinstance(q, (int, float)):
         return log(Quaternion(q, 0.0, 0.0, 0.0))
 
-    else:
-        return TypeError
+    raise TypeError("Cannot take the quaternion logarithm of a "
+                 + f"{q.__class__.__qualname__}.")
 
 
 def log10(q: Quaternion or float) -> Quaternion:
@@ -101,6 +152,11 @@ def log10(q: Quaternion or float) -> Quaternion:
 
 def sqrt(q: Quaternion or float) -> Quaternion:
     """Return the square root of the quaternion."""
+    # Negative real numbers have an infinite number of square roots in
+    # the quaternion number system. These conditional statements check
+    # if the input is a negative real number. If the input is not a
+    # negative real number, this function returns the input raised to
+    # 0.5. This should always return a Quaternion object.
     sqrt_real = 0.0
     if isinstance(q, Quaternion):
         if q.is_scalar() and q.real < 0:
@@ -110,6 +166,8 @@ def sqrt(q: Quaternion or float) -> Quaternion:
         if q < 0:
             real = q
             sqrt_real = (-q)**0.5
+        else:
+            q = Quaternion(q)
 
     if sqrt_real:
         if len(str(sqrt_real)) <= 4:
@@ -118,9 +176,9 @@ def sqrt(q: Quaternion or float) -> Quaternion:
             sqrt_string = f"{sqrt_real:.6f}..."
 
         raise ValueError(
-            "Negative real quaternions have an infinite number of square roots.\n"
-            + f"The square root of {real} is the sphere of radius {sqrt_string} "
-            + "centered at the origin.")
+            "Negative real quaternions have an infinite number of square "
+            + f"roots.\nThe square root of {real} is the sphere of radius "
+            + f"{sqrt_string} centered at the origin.")
 
     return pow(q, 0.5)
 
@@ -136,35 +194,40 @@ def rotate3d(
     (3) values and rotate it by an angle around a given axis vector.
 
     Parameters:
-        point: The point to rotate. The format for the coordinates is ``(x, y, z)``.
-        angle: The angle of rotation. By default, angle is set to be input in
-            degrees. See the **degrees** parameter if you want to use radians instead.
-        axis: The axis to rotate the point around. By default, this is the
-            z-axis ``(0, 0, 1)``.
-        rounding: The number of decimal points the result will be rounded to.
-            Default value is -1, which does not round the end result.
-        degrees: When set to ``True``, this function interprets the parameter
-            **angle** as degrees. Set this parameter to ``False`` to use angles
-            in radians. Default is ``True``.
+        point: The point to rotate. The format for the coordinates is
+            ``(x, y, z)``.
+        angle: The angle of rotation. By default, angle is set to be
+            input in degrees. See the **degrees** parameter if you want
+            to use radians instead.
+        axis: The axis to rotate the point around. By default, this is
+            the z-axis ``(0, 0, 1)``.
+        rounding: The number of decimal points the result will be
+            rounded to. Default value is -1, which does not round the
+            end result.
+        degrees: When set to ``True``, this function interprets the
+            parameter **angle** as degrees. Set this parameter to
+            ``False`` to use angles in radians. Default is ``True``.
 
-    For the point and axis parameters, if only one value is given, the value
-    will be assumed to be an x-coordinate with the y- and z-coordinates
-    equal to zero (0). If two values are given, they will be assumed to
-    be x- and y-coordinates with the z-coordinate equal to zero (0).
+    For the point and axis parameters, if only one value is given, the
+    value will be assumed to be an x-coordinate with the y- and
+    z-coordinates equal to zero (0). If two values are given, they will
+    be assumed to be x- and y-coordinates with the z-coordinate equal
+    to zero (0).
     """
     if len(point) <= 3 and len(axis) <= 3:
         p = Quaternion(0, *_makeListLen3(point))
 
         axis_i, axis_j, axis_k = _makeListLen3(axis)
-        if _hypot(axis_i, axis_j, axis_k) == 0:
+        if _hypot(axis_i, axis_j, axis_k) < 1e-12:
             raise ValueError(
                 'The axis to rotate around must be a nonzero vector.')
 
-        q = Quaternion.from_angle(angle*0.5, (axis_i, axis_j, axis_k), degrees=degrees)
+        q = Quaternion.from_angle(
+                angle*0.5, (axis_i, axis_j, axis_k), degrees=degrees)
         if abs(q) != 1.0:
             q = q.versor  # Ensures q is a unit vector.
         p_prime = q * p * q.inverse()
-        if rounding != -1:
+        if rounding >= 0:
             p_prime = round(p_prime, rounding)
 
         i_prime, j_prime, k_prime = p_prime.get_vector_components()
@@ -187,8 +250,8 @@ def dot_product(
     """
     Return the dot product of two vectors.
 
-    Because this uses quaternions to calculate, this only works for vectors
-    up to three (3) dimensions.
+    Because this uses quaternions to calculate, this only works for
+    vectors up to three (3) dimensions.
     """
     i1, j1, k1 = _makeListLen3(vector1)
     i2, j2, k2 = _makeListLen3(vector2)
@@ -202,8 +265,8 @@ def cross_product(
     """
     Return the cross product of two vectors.
 
-    Because this uses quaternions to calculate, this only works for vectors
-    up to three (3) dimensions.
+    Because this uses quaternions to calculate, this only works for
+    vectors up to three (3) dimensions.
     """
     i1, j1, k1 = _makeListLen3(vector1)
     i2, j2, k2 = _makeListLen3(vector2)
@@ -224,25 +287,25 @@ def rotate_Euler(
     then pitch, then roll.
 
     Parameters:
-        point: The point to rotate. The format for the coordinates is ``(x, y, z)``.
+        point: The point to rotate. The format for the coordinates is
+            ``(x, y, z)``.
         yaw: The angle of rotation around the z-axis.
-        pitch: The angle of rotation around the y'-axis. The y'-axis is the y-axis
-            after the yaw rotation has been applied.
-        roll: The angle of rotation around the x"-axis. The x"-axis is the x-axis
-            after both the yaw and pitch rotations.
-        x_axis: The initial x-axis of the coordinate system that **point** belongs
-            to. Default value is ``(1, 0, 0)``.
-        z_axis: The initial z-axis of the coordinate system that **point** belongs
-            to. Default value is ``(0, 0, 1)``.
-        degrees: When set to ``True``, this function interprets the parameter
-            **angle** as degrees. Set this parameter to ``False`` to use angles
-            in radians. Default is ``True``.
-
-    .. versionadded:: 1.1.0
+        pitch: The angle of rotation around the y'-axis. The y'-axis is
+            the y-axis after the yaw rotation has been applied.
+        roll: The angle of rotation around the x"-axis. The x"-axis is
+            the x-axis after both the yaw and pitch rotations.
+        x_axis: The initial x-axis of the coordinate system that
+            **point** belongs to. Default value is ``(1, 0, 0)``.
+        z_axis: The initial z-axis of the coordinate system that
+            **point** belongs to. Default value is ``(0, 0, 1)``.
+        degrees: When set to ``True``, this function interprets the
+            parameter **angle** as degrees. Set this parameter to
+            ``False`` to use angles in radians. Default is ``True``.
     """
     # yaw: rotate around z-axis
     # pitch: rotate around y'-axis
     # roll: rotate around x"-axis
+    # TODO: Code all standards of Euler angles into this function.
     q_yaw = Quaternion.from_angle(yaw*0.5, z_axis)
     x_prime_axis = (q_yaw * Quaternion(0, *x_axis).versor
                     * q_yaw.inverse()).get_vector_components()
@@ -254,7 +317,11 @@ def rotate_Euler(
 
     q_point = Quaternion(0, *point)
     q_p_prime = (
-        q_roll * (q_pitch * (q_yaw * q_point * q_yaw.inverse()) * q_pitch.inverse())
-        * q_roll.inverse())
+        q_roll * (
+            q_pitch * (
+                q_yaw * q_point * q_yaw.inverse()
+            ) * q_pitch.inverse()
+        ) * q_roll.inverse()
+    )
 
     return q_p_prime.get_vector_components()
